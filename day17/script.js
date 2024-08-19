3780644b090d10429014fbb3bbacfb82dbf002ae
Pratyush Nishchal <63450333+pratyushnishchal@users.noreply.github.com>
2024-08-19 22:20:30 +0530

    Add files via upload

---
+++ day17/script.js
@@ -0,0 +1,66 @@
+// console.log("hey")
+
+// --hoisting
+// a=10
+// console.log(a)
+// var a;
+
+// --changing the value of a
+// a=10
+// console.log(a)
+// a=20;
+// console.log(a)
+
+// --using const keyword to change the value of a
+// const a=10
+// console.log(a)
+// a=20;
+// console.log(a)
+
+// WAP to check whether hoisting will be supported by let for printing addition of 2 numbers or not
+
+// x=10;
+// y=20;
+// let x;
+// let y;
+// console.log(x+y)
+
+// WAP to print const of 2 numbers for addition
+
+// const x1=10
+// const x2=20
+// sum=x1+x2
+// console.log(x1+x2)
+
+// primitive type
+// number type
+let a=10
+console.log(a,typeof(a))
+// string type
+let b='20'
+console.log(b,typeof(b))
+//bigint type
+c=1234567890n
+console.log(c,typeof(c))
+// null
+d=null
+console.log(d,typeof(d))
+// undefined
+let e;
+console.log(e,typeof(e))
+// boolean type
+let f=false;
+console.log(f,typeof(f))
+// Symbol type
+let g=Symbol('pratyush')
+console.log(g,typeof(g))
+
+// non primitive type
+
+// let person={
+//     name:'pratyush'
+// }
+// dot operator
+// console.log(person.name)
+// bracket notation
+// console.log(person['name'])
