685ea1783c26fb2716633e3f9da62752b47ccf14
Pratyush Nishchal <63450333+pratyushnishchal@users.noreply.github.com>
2024-08-25 17:17:17 +0530

    Add files via upload

---
+++ day19/script.js
@@ -0,0 +1,80 @@
+// let num=11
+// console.log(num%5==0? "Hi":"Bye")
+
+// let person={
+//     name:"pratyush",
+//     age:23,
+//     course:{coursename:"java",duration:2},
+//     fees:50000
+// }
+
+// console.log(person)
+// delete person.course.duration
+// console.log(person)
+
+//if condition
+
+//wap to check whether a given string is concated if its concatinated then display "hey"  
+
+const prompt = require("prompt-sync")();
+// let s1=prompt()
+// let s2=prompt()
+// let s3=s1+s2
+// if(s3){
+//     console.log("hi")
+// }
+
+//wap to check given  numbers is signel digit if it is single digit .display that digit itsself 
+
+// let a=prompt(parseInt())
+// if(a>0 || a<=9){
+//     console.log(a," is a single digit number")
+// }
+
+//if else condition 
+
+//wap if the given number is divisible by 7 and 5 then it say 'excler' else 'not excelr'
+
+// let num=prompt(parseInt())
+// if(num%7==0 && num%5==0){
+//     console.log("Excelr")
+// }
+// else{
+//     console.log("Not Excelr")
+// }
+
+// wap to check whether the given number is single didit,double or triple digit and if it is 4 digits just exit
+
+// let a=prompt(parseInt())
+// if(a>=100 && a<=999){
+//     console.log(a," is a triple digit number")
+// }
+// else if(a>=10 && a<=99){
+//     console.log(a," is a double digit number")
+// }
+// else if(a>=0 && a<=9){
+//     console.log(a," is a single digit number")
+// }
+// else{
+//     console.log("break")
+// }
+
+//check whether the given number is even or odd 
+
+// let b=prompt(parseInt())
+// if(b%2==0){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+//wap to check whther a given number is positive or negative
+
+let c=prompt(parseInt())
+if(c<0){
+    console.log("negative")
+}
+else{
+    console.log("positive")
+}
