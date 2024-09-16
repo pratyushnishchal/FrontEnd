775e7b4aa31158cc05319fe75654586a08059fb0
Pratyush Nishchal <63450333+pratyushnishchal@users.noreply.github.com>
2024-09-16 22:13:48 +0530

    Add files via upload

---
+++ day31/script.js
@@ -0,0 +1,22 @@
+// const prompt = require("prompt-sync")();
+// class Employee{
+//     constructor(name,age,salary){
+//         this.name=name
+//         this.age=age
+//         this.salary=salary
+//     }
+//     calculateSalary(){
+//         let monthly_bonus=parseInt(prompt("Enter your monthly bonus: "))
+//         let finalsalary=monthly_bonus+this.salary
+//         console.log(`The ${this.name} salary is ${finalsalary}`)
+//     }
+//     getED(){
+//         console.log(`Employee Name is ${this.name} and Employee age is ${this.age}`)
+//         this.calculateSalary()
+//     }
+
+// }
+
+// let P=new Employee("pratyush",23,35000)
+// P.getED()
+
