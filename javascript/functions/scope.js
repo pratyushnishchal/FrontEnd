5554af426e5846d1e9af880ceb5ccba90df3897b
Pratyush Nishchal <pratyushnishchal@gmail.com>
2024-11-10 23:45:42 +0530

    updated the scope.js code

--- javascript/functions/scope.js
+++ javascript/functions/scope.js
@@ -1,11 +1,27 @@
 console.log("We are learning scope")
 
 // global scope
-let globalVar=10;
+// let globalVar=10;
 
-let display=()=>{
-    console.log("Inside the display function")
-    console.log(`Inside the function ${globalVar}`)
+// let display=()=>{
+//     console.log("Inside the display function")
+//     console.log(`Inside the function ${globalVar}`)
+// }
+// display();
+// console.log(globalVar)
+
+// local scope
+
+// let display=()=>{
+//     let localVar=10;
+//     console.log("Inside the display function")
+//     console.log(`Inside the function ${localVar}`)
+// }
+// display();
+// console.log(localVar)
+
+let blockVar=10;
+if(true){
+    console.log(blockVar)
 }
-display();
-console.log(globalVar)
+console.log(blockVar)
