e16abb518ebbe0b400c3344cbc01075c337b5dfe
Pratyush Nishchal <pratyushnishchal@gmail.com>
2024-11-12 12:11:03 +0530

    updated setTimeout and setInterval

--- javascript/DateTime/setInterval_setTimeout.js
+++ javascript/DateTime/setInterval_setTimeout.js
@@ -3,6 +3,9 @@
 //     console.log("Hello world")
 // }, 1000);
 
+// Stopping setInterval(): Use clearInterval(intervalId) to stop the interval.
+
+// setTimeout() :-is used to execute a function after a specified delay (in milliseconds), but it only runs once.
 setTimeout(() => {
     console.log("This runs once after 3 sec")
 }, 3000);
